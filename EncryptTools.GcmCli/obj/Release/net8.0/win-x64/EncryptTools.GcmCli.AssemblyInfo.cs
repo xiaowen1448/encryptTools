843c52ee07e1eff8ed930436daafd846e5ee171e
xiaowen1448 <wenxiaomst@outlook.com>
2026-03-25 16:21:14 +0800

    清理无用文件

--- EncryptTools.GcmCli/obj/Release/net8.0/win-x64/EncryptTools.GcmCli.AssemblyInfo.cs
+++ EncryptTools.GcmCli/obj/Release/net8.0/win-x64/EncryptTools.GcmCli.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EncryptTools.GcmCli")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bda2197278ffcc4c328f753cbad6defaef33a37c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f734946fa196ba8421b87ca076e789752610eb2")]
 [assembly: System.Reflection.AssemblyProductAttribute("EncryptTools.GcmCli")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EncryptTools.GcmCli")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
